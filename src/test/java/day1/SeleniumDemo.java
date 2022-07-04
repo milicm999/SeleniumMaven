76d35553af48a7b148cae7ce3c9c23201e8d0a44
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-04 14:59:42 +0200

    Started with Selenium testing

---
+++ src/test/java/day1/SeleniumDemo.java
@@ -0,0 +1,63 @@
+package day1;
+
+import com.sun.org.apache.bcel.internal.generic.Visitor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SeleniumDemo {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\milicm\\OneDrive - Comtrade Group\\Desktop\\QA assignment_Marija_Milic\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        driver.get("https://demo.guru99.com/test/newtours/");
+
+        System.out.println(driver.getTitle());
+        //Verifying title
+        if(driver.getTitle().equals("Welcome: Mercury Tours"))
+            System.out.println("Title verfied");
+        else
+            System.out.println("Verifying title test failed.");
+
+
+        //Testing LogIn
+//        Select username= new Select(driver.findElement(By.name("User Name")));
+//        Select password= new Select(driver.findElement(By.name("Password")));
+
+//        username.selectByVisibleText("text");
+//        password.selectByVisibleText("text");
+
+//        driver.findElement(By.name("userName")).sendKeys(Keys.COMMAND + "maja99");
+//        driver.findElement(By.name("password")).sendKeys(Keys.COMMAND + "maja99");
+//        driver.findElement(By.name("submit")).click();
+        WebElement username=driver.findElement(By.name("userName"));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.name("userName")));
+        username.sendKeys(Keys.COMMAND + "maja99");
+        WebElement password= driver.findElement(By.name("password"));
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.name("password")));
+        password.sendKeys(Keys.COMMAND + "maja99");
+        WebElement submit= driver.findElement(By.name("submit"));
+        submit.click();
+
+        System.out.println(driver.getTitle());
+
+        if(driver.getTitle().equals("Login: Mercury Tours"))
+            System.out.println("Log in was successful, test pass");
+        else
+            System.out.println("Log in failed, test failed");
+
+    }
+}
+
+
